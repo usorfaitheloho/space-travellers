2a4a0384e36c9414c24a77025c1de73bd3a38e6f
bushmusi <bushera.mestofa@gmail.com>
2022-04-11 17:17:40 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Rocket from './component/rockets/rocket';
+import Mission from './component/missions/mission';
+import MyProfile from './component/my-profile/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        
+        <Routes>
+          <Route path="/rockets" element={<Rocket />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/profile" element={<MyProfile />} />
+          <Route path="/" exact element={<Rocket />} />
+        </Routes>
+        </div>
+    </Router>
   );
 }
 
