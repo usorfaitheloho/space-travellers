a1631100e23e209f575a6b774e0ed4b3e81e5147
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-12 00:52:08 +0430

    Add logo image

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,26 +6,21 @@ const Header = () => (
   <header className="header">
     <div className="left-header">
       <h1 className="logo">
-        <img src="../../image/logo.png" alt="logo" />
+        <img src="../Header/image/logo.png" alt="logo" />
       </h1>
-
       <nav className="navigation">
         <h2>Space Traveler's Hub</h2>
-        {/*  */}
       </nav>
     </div>
     <div className="right-header">
       <li>
-        {" "}
         <Link to="/">Rockets</Link>
       </li>
       <li>
-        {" "}
         <Link to="/mission">Mission</Link>
       </li>
       <span>|</span>
       <li>
-        {" "}
         <Link to="/profile">MyProfile</Link>
       </li>
     </div>
