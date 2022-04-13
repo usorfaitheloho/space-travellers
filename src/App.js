0c0d3bd46bd3a6d76fedd18003d08322710bdbfc
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-13 19:14:38 +0430

    Add stor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-impo
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './App.css';
 import Rocket from './component/rockets/rocket';
 import Mission from './component/missions/mission';
 import MyProfile from './component/my-profile/profile';
-import Dragon from './component/Dragons/DragonCard';
+import { fetchPostsRequestDragons } from './redux/Dragons/dragons';
+import Dragons from './component/Dragons/Dragons';
 import Header from './component/Header/Header';
-import { useDispatch } from 'react-redux';
+
 
 function App() {
   const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(fetchPostsRequestDragons());
+  },[]);
+  const dragons = useSelector((state) => state.dragon.dragons);
   return (
     <Router>
       <div className="App">
@@ -18,7 +25,10 @@ function App() {
 
           <Route path="/rockets" element={<Rocket />} />
           <Route path="/mission" element={<Mission />} />
-          <Route path="/Dragons" element={<Dragon />} />
+          <Route path="/Dragons"  >
+          <Dragons dragons={dragons} />
+          </Route>
+          
           <Route path="/profile" element={<MyProfile />} />
           <Route path="/" exact element={<Rocket />} />
         </Routes>
