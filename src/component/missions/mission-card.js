afd46bdd6ecd882375a23d3e8f3bfc7035538a45
bushmusi <bushera.mestofa@gmail.com>
2022-04-14 17:23:16 +0300

    Add update action

--- src/component/missions/mission-card.js
+++ src/component/missions/mission-card.js
@@ -1,8 +1,11 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { updateMembership } from '../../redux/missions/mission';
 
 export default function missionItem({ itemDetail }) {
+  const dispatch = useDispatch();
   const len = itemDetail.payload_ids.length;
+  
   return (
     <tr>
       <td>
@@ -15,7 +18,7 @@ export default function missionItem({ itemDetail }) {
         <button type="button" className={len >= 3 ? 'activeStatus' : ''}>{len >= 3 ? 'Active member' : 'Not a member'}</button>
       </td>
       <td className="btnDataBox">
-        <button type="button" className={`rightBtn ${len >= 3 ? 'activeAction' : ''}`}>
+        <button type="button" className={`rightBtn ${len >= 3 ? 'activeAction' : ''}`} onClick={() => dispatch(updateMembership(itemDetail.mission_id))}>
           { len >= 3 ? 'Leave mission' : 'Join mission'}
         </button>
       </td>
