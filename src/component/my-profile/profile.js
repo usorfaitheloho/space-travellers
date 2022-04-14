31cb8c73e2db98381a22218b2c928a6fa71f3148
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-13 22:16:46 -0700

    Implement changes

--- src/component/my-profile/profile.js
+++ src/component/my-profile/profile.js
@@ -1,5 +1,25 @@
-export default function MyProfile() {
-  return (
-    <h1 style={{ textAlign: 'center' }}>Welcome to MyProfile page</h1>
-  );
-}
+import React from 'react';
+import './profile.css';
+
+const MyProfile = () => (
+  <div className="profile">
+    <section className="my-missions-sec">
+      <h3 className="my-heading"> My Missions</h3>
+      <div className="my-missions">
+        <p className="my-mission">Telstar</p>
+        <p className="my-mission">SES</p>
+        <p className="my-mission">AsiaSat</p>
+        <p className="my-mission">So50</p>
+      </div>
+    </section>
+    <section className="my-rockets-sec">
+      <h3 className="my-heading"> My Rockets </h3>
+      <div className="my-rockets">
+        <p className="my-rocket">Falcon9</p>
+        <p className="my-rocket">Falcon</p>
+        <p className="my-rocket">Starship</p>
+      </div>
+    </section>
+  </div>
+);
+export default MyProfile;
