c80f920e97d4bb0cdb3310e872e7f200c7a278d5
usorfaitheloho <usorfaitheloho@gmail.com>
2022-04-13 22:54:22 -0700

    Add a function for handling reservations

--- src/component/my-profile/profile.js
+++ src/component/my-profile/profile.js
@@ -7,6 +7,14 @@ const MyProfile = () => {
   const myRockets = useSelector((state) => state.rockets);
   const myReserveRockets = myRockets.filter((rocket) => rocket.reserved = true);
 
+  const handleReservations = () => {
+    if (myReserveRockets.length) {
+      return myReserveRockets.map((rocket) => {
+        <p key={rocket.id} className="my-rocket">{rocket.name}</p> 
+      })
+    }
+  }
+
   return(
     <div className="profile">
     <section className="my-missions-sec">
@@ -21,9 +29,9 @@ const MyProfile = () => {
     <section className="my-rockets-sec">
       <h3 className="my-heading"> My Rockets </h3>
       <div className="my-rockets">
-        <p className="my-rocket">Falcon9</p>
-        <p className="my-rocket">Falcon</p>
-        <p className="my-rocket">Starship</p>
+          { 
+          handleReservations() 
+          }
       </div>
     </section>
   </div>
