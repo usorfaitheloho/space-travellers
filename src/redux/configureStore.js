c5bb2d1420413e5fa974e001e6eb4ea9ce893f66
bushmusi <bushera.mestofa@gmail.com>
2022-04-14 09:50:36 +0300

    fix crlf

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,8 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import missionReducer, { fetchMissionSuccess } from './missions/mission';
 
-import logger from 'redux-logger';
 import profileReducer from './profiles/profile';
 import rocketReducer from './rockets/rockets';
 
