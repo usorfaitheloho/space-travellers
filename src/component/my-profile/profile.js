97fd86a7a1917aa44b2ff265fab7f1af3bba1544
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-15 09:22:58 +0430

    fix style dragon in profile page

--- src/component/my-profile/profile.js
+++ src/component/my-profile/profile.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import './profile.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import "./profile.css";
 
 const MyProfile = () => {
   const myRockets = useSelector((state) => state.rockets);
-  const myReserveRockets = myRockets.rocketsData.filter((rocket) => rocket.reserved);
+  const myReserveRockets = myRockets.rocketsData.filter(
+    (rocket) => rocket.reserved
+  );
 
   const handleReservations = () => {
     if (myReserveRockets.length) {
       return myReserveRockets.map((rocket) => (
-        <p key={rocket.id} className="my-rocket">{rocket.name}</p>
+        <p key={rocket.id} className="my-rocket">
+          {rocket.name}
+        </p>
       ));
     }
     return <h2>No Reserved Rockets</h2>;
   };
 
+  
+  const myDragons = useSelector((state) => state.dragons);
+  const myReserveDragons = myDragons.DragonsData.filter(
+    (dragon) => dragon.reserved
+  );
+  const handleDragonReservation = () => {
+    if (myReserveDragons.length) {
+      return myReserveDragons.mag((dragon) => {
+        <p key={dragon.id} className="my-dragon">
+          {dragon.name}
+        </p>;
+      });
+    }
+    return <h2> No reserved Dragons</h2>;
+  };
 
   return (
     <div className="profile">
@@ -29,17 +48,11 @@ const MyProfile = () => {
       </section>
       <section className="my-rockets-sec">
         <h3 className="my-heading"> My Rockets </h3>
-        <div className="my-rockets">
-          {
-          handleReservations()
-          }
-        </div>
+        <div className="my-rockets">{handleReservations()}</div>
       </section>
-      <section className='my-dragons-sec'>
-        <h3 className='my-heading'>My Dragons</h3>
-        <div className='my-dragons'>
-          {/* handle */}
-        </div>
+      <section className="my-dragons-sec">
+        <h3 className="my-heading">My Dragons</h3>
+        <div className="my-dragons">{handleDragonReservation()}</div>
       </section>
     </div>
   );
