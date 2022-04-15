5d49abaa87fdcb941f00cac11eeba63cf948c213
bushmusi <bushera.mestofa@gmail.com>
2022-04-15 15:48:13 +0300

    Add header test

---
+++ src/component/Header/__tests__/header-screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../Header';
+
+test('Header renderes correctly', () => {
+    const tree = renderer.create(
+        <Router>
+            <Header />
+        </Router>
+    )
+})
